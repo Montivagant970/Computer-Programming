26917a2fda3cc843924f66a68b7499e740c5f8b9
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-03-13 16:39:24 +0100

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -2,6 +2,15 @@ import streamlit as st
 st.header("hello world")
 st.text("from Bressanone")
 
+import json, requests 
+import streamlit as st
+APIkey = 'b0dc5ff479faf43dff849169f51ad2b0'
+location = st.text_input('Write a city name:', '')
+url = 'http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=' + APIkey + '&units=metric'
+response = requests.get(url)  
+weatherData = json.loads(response.text)
+st.text('Today\'s high is:', ['main']['temp_max'], 'and today\'s low is:', ['main']['temp_min'])
+
 title = st.text_input("Please insert a movie title:", '')
 st.write('The current movie title is:', title)
 
