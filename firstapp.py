0cd9132e969c12f6826558eb70d0be34013f4699
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-03-13 16:31:12 +0100

    Update firstapp.py

--- firstapp.py
+++ firstapp.py
@@ -3,4 +3,11 @@ st.header("hello world")
 st.text("from Bressanone")
 
 title = st.text_input("Please insert a movie title:", '')
-st.write('The current movie title is', title)
+st.write('The current movie title is:', title)
+
+import streamlit as st
+genre = st.radio("What's your favorite movie genre?",('Comedy', 'Drama', 'Documentary'))
+if genre == 'Comedy':
+     st.write('You selected comedy.')
+ else:
+     st.write("You didn't select comedy.")
