e91dd8b65a361fd3be3243266f8541e5da2c8878
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-04-07 13:12:56 +0200

    Update translator.py

--- translator.py
+++ translator.py
@@ -5,5 +5,6 @@ translator = Translator()
 st.header("Welcome to an awful online translator!")
 word = st.text_input('Please enter a word:')
 targ_lang = st.text_input('Enter a language or a two letter language code for translation:')
-lang_detect = translator.translate(word, dest=targ_lang)
-st.write('The translation is:', lang_detect.text)
+if (word and targ_lang):
+  lang_detect = translator.translate(word, dest=targ_lang)
+  st.write('The translation is:', lang_detect.text)
