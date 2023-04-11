535d01a547f5c28e420db365f2f0930c451cd49c
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-04-11 15:57:51 +0200

    Update translator_tts.py

--- translator_tts.py
+++ translator_tts.py
@@ -8,9 +8,10 @@ st.header("Here's a translator and text-to-speech all in one app!")
 text_input = st.text_input('Write some text to be translated and made into an audio:')
 lang_code = st.text_input('Enter a two letter language code for translation (en - English, de - Deutsch, it - italiano):')
 
-text_trans = translator.translate(text_input, dest=lang_code)
+if (text_input and lang_code):
+  text_trans = translator.translate(text_input, dest=lang_code)
 
-tts1=gTTS(text=text_trans.text, lang=lang_code)
-tts1.save('user_audio.mp3')
+  tts1=gTTS(text=text_trans.text, lang=lang_code)
+  tts1.save('user_audio.mp3')
 
-st.audio(data='user_audio.mp3', format='audio/mp3', start_time=0)
+  st.audio(data='user_audio.mp3', format='audio/mp3', start_time=0)
