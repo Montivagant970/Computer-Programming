f4760934add286295b8087e9d1c6abeaa95d414f
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-04-15 18:46:17 +0200

    Update languageresource.py

--- languageresource.py
+++ languageresource.py
@@ -1,1 +1,20 @@
+import streamlit as st
+from googletrans import Translator
+translator = Translator()
 
+st.header("Speed Reference")
+
+criterion = st.multiselect("Which resources do you need?", ("Translation", "Pronunciation", "Same Language Definition", "Conjugations/Declinations", "Etymology", "All"), default=None)
+
+source_lang = st.text_input('Translate from: (de - German, it - Italian, en - English')
+dest_lang = st.text_input('Translate into: (de - German, it - Italian, en - English')
+
+st.text('Please enter a word:')
+user_input = st.text_input()
+#TO ADD SPEECH TO TEXT 
+
+if (user_input):
+  for x in criterion:
+    if x == "Translation":
+      input_trans = translator.translate(user_input, src = source_lang, dest = dest_lang)
+      st.write('Translation:', input_trans.text)
