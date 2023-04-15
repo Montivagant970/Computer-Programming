1172b5a93505079e6b507469d23695bb55f98bd5
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-04-15 18:57:38 +0200

    Update languageresource.py

--- languageresource.py
+++ languageresource.py
@@ -1,10 +1,11 @@
 import streamlit as st
 from googletrans import Translator
+from gtts import gTTS 
 translator = Translator()
 
 st.header("Speed Reference")
 
-criterion = st.multiselect("Which resources do you need?", ("Translation", "Pronunciation", "Same Language Definition", "Conjugations/Declinations", "Etymology", "All"), default=None)
+criterion = st.multiselect("Which resources do you need?", ("Translation", "Pronunciation", "Same-Language Definition", "Conjugations/Declensions", "Etymology", "All"), default=None)
 
 source_lang = st.text_input('Translate from: (de - German, it - Italian, en - English')
 dest_lang = st.text_input('Translate into: (de - German, it - Italian, en - English')
@@ -18,3 +19,27 @@ if (user_input):
     if x == "Translation":
       input_trans = translator.translate(user_input, src = source_lang, dest = dest_lang)
       st.write('Translation:', input_trans.text)
+
+    if x == "Pronunciation":
+      input_trans = translator.translate(user_input, src = source_lang, dest = dest_lang)
+
+      tts = gTTS(text = input_trans.text, lang = dest_lang)
+      tts.save('user_audio.mp3')
+
+      st.audio(data = 'user_audio.mp3', format = 'audio/mp3', start_time=0)
+
+    if x == "Same-Language Definition":
+    if x == "Conjugations/Declensions":
+    if x == "Etymology":
+    if x == "All":
+      #Translator
+      input_trans = translator.translate(user_input, src = source_lang, dest = dest_lang)
+      st.write('Translation:', input_trans.text)
+
+      #Pronunciation
+      tts = gTTS(text = input_trans.text, lang = dest_lang)
+      tts.save('user_audio.mp3')
+
+      st.audio(data = 'user_audio.mp3', format = 'audio/mp3', start_time=0)
+
+      #Cont. here
