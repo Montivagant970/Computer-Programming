74ab6d3bde807782968b022e96e71a2ae6f151a2
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-04-17 18:58:12 +0200

    Update languageresource.py

--- languageresource.py
+++ languageresource.py
@@ -23,21 +23,21 @@ for x in source_lang:
   elif source_lang == 'english':
     source_lang_trans = 'en'
   else:
-    st.write("Your language is not available at this time."
+    st.write("Your language is not available at this time.")
 
-#if (criterion):
-  #for z in criterion:
-    #if z == "Translation" or "All":
-      #dest_lang = st.text_input('Into which language would you like to translate?:')
-      #dest_lang = dest_lang.lower()
-      #if (dest_lang):
-        #for y in dest_lang:
-          #if dest_lang == 'german' or 'deutsch':
-            #dest_lang_trans = 'de'
-         # elif dest_lang == 'italian' or 'italiano':
-           # dest_lang_trans = 'it'
-    #      elif dest_lang == 'english':
-  #          dest_lang_trans = 'en'
+if (criterion):
+  for z in criterion:
+    if z == "Translation" or "All":
+      dest_lang = st.text_input('Into which language would you like to translate?:')
+      dest_lang = dest_lang.lower()
+      if (dest_lang):
+        for y in dest_lang:
+          if dest_lang == 'german' or 'deutsch':
+            dest_lang_trans = 'de'
+          elif dest_lang == 'italian' or 'italiano':
+            dest_lang_trans = 'it'
+          elif dest_lang == 'english':
+            dest_lang_trans = 'en'
 
 st.header('PLEASE ENTER A WORD:')
 user_input = st.text_input('')
