252b41d623ccc44f3e8af063a88053735eacc357
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-05-17 09:44:18 +0200

    Update languageresource.py

--- languageresource.py
+++ languageresource.py
@@ -129,14 +129,15 @@ if (user_input):
       input_trans = translator.translate(user_input, src = working_lang, dest = trans_lang)
       st.subheader('Translation:')
       st.write('Translation:', input_trans.text)
+      input_trans_text = input_trans.text
+      input_trans_lower = input_trans_text.lower()
       
       #Pronunciation
       tts = gTTS(text = input_trans.text, lang = trans_lang)
       tts.save('user_audio.mp3')
 
       st.subheader("Pronunciation:")
-      gramm_info = parser.fetch(input_trans.text, trans_lang2)
-      st.write(gramm_info[0]['pronunciations'])
+      gramm_info = parser.fetch(input_trans_lower, trans_lang2)
       st.write(gramm_info[0]['pronunciations']['text'][0])
       st.audio(data = 'user_audio.mp3', format = 'audio/mp3', start_time=0)
 
