9ed7673bc5a70a6038fd53777ae3a8470ae6454e
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-05-17 11:19:48 +0200

    Update languageresource.py

--- languageresource.py
+++ languageresource.py
@@ -31,7 +31,7 @@ trans_lang = None
 #starter material
 st.title("Multilingual Speed Reference")
 
-criterion = st.multiselect("Let's get started! Which resources do you need?", ("Translation", "Pronunciation", "Definition", "Grammatical Information (Conjugations/Declensions)", "Etymology", "Example, i.e. Word in Context", "All"), default=None)
+criterion = st.multiselect("Let's get started! Which resources do you need?", ("Translation", "Pronunciation", "Definition", "Grammatical Information, i.e. Conjugations/Declensions", "Etymology", "Example, i.e. Word in Context", "All"), default=None)
 if criterion:
   for x in criterion:
     if x == "All":
