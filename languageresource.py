050c70ec54984b07574747453d396175db9bb680
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-05-17 11:23:27 +0200

    Update languageresource.py

--- languageresource.py
+++ languageresource.py
@@ -106,7 +106,7 @@ if (user_input):
     if x == "Definition":
       def_info = parser.fetch(user_input, working_lang2)
       st.subheader('Definition:')
-      st.write(gramm_info[0]['definitions'][0]['text'][1:])
+      st.write(def_info[0]['definitions'][0]['text'][1:])
     else:
       pass
     
