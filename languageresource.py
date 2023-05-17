557b3380dea8a1c1f7f7f5b279151029f1689b95
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-05-17 10:47:50 +0200

    Update languageresource.py

--- languageresource.py
+++ languageresource.py
@@ -104,10 +104,12 @@ if (user_input):
     else:
       pass
 
-    #if x == "Same-Language Definition":
-    gramm_info = parser.fetch(user_input, working_lang2)
+    if x == "Same-Language Definition":
+      gramm_info = parser.fetch(user_input, working_lang2)
       st.subheader('Grammatical Information:')
       st.write(gramm_info[0]['definitions'][0]['text'][1:])
+    else:
+      pass
     
     if x == "Grammatical Information (Conjugations/Declensions)":
       gramm_info = parser.fetch(user_input, working_lang2)
