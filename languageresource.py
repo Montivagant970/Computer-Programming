b7e54d068b5dc2e5a391fc1b9be8a30c77d6f2d9
Montivagant970 <127765734+Montivagant970@users.noreply.github.com>
2023-05-17 11:21:27 +0200

    Update languageresource.py

--- languageresource.py
+++ languageresource.py
@@ -41,7 +41,7 @@ else:
 
 if criterion:
   lang_option = st.selectbox("Please enter your Working Language?:", lang_options.values())
-  st.caption("TIP: your Working Language is the language of the words you will be inserting into the program. For example, if I want to translate the word 'Hund', then the Working Language is 'German'.")
+  st.caption("TIP: your Working Language is the language of the words you will be inserting into the program. For example, if I want to translate the word 'Hund,' then the Working Language is 'German.'")
 
 if lang_option:
   st.header('PLEASE ENTER A WORD:')
